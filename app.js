7f9a7333d285d8c555da65348dca39b2995df610
mkumary <manoj.kumar@lrn.com>
2016-12-14 18:52:00 +0530

    chaneges to add html

--- app.js
+++ app.js
@@ -3,23 +3,22 @@ var axios = require('axios');
 app.set('port', (process.env.PORT || 4000));
 
 app.get('/', function(req,res){
-    res.send('hello');
-})
-
-app.get('/getArtist/:name', function(req,res){
-    var name = req.params.name;
-    console.log(name);
-    getArtist(name).then(function(result){
-        console.log('hello');
-       console.log(result.data);
-        res.send(result.data);
-    })
+    res.setHeader('Content-Type', 'text/html');
+    res.sendFile('index.html',{root: __dirname });
 });
+
 
 app.get('/searchArtist/:searchText', function(req,res){
    var searchText  = req.params.searchText;
-    searchArtist(searchText).then(function(result){
-        res.send(getArtistNameList(result.data).join('\n'));
+    searchArtist(searchText).then(function(searchresult){
+        getSimilarArtists(searchText).then(function(similarArtist){
+                var hello =  {
+                    "searchResult" :getArtistNameList(searchresult.data),
+                    "similarArtists":similarArtist.data.similarartists.artist
+                };
+              res.send(hello);
+            }
+        );
     });
 });
 
@@ -31,12 +30,32 @@ app.get('/searchTrack/:searchText', function(req,res){
 });
 
 
+
+
+
+
+
+
+
+
+app.get('/getArtist/:name', function(req,res){
+    var name = req.params.name;
+    console.log(name);
+    getArtist(name).then(function(result){
+        console.log('hello');
+        console.log(result.data);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(result.data));
+    })
+});
+
+
 function getArtistNameList(data){
     var names = [];
      data.results.artistmatches.artist.forEach(function(element){
         names.push(element.name);
     });
-    return names;
+    return data.results.artistmatches.artist;
 }
 
 
@@ -45,11 +64,17 @@ function getTrackList(result){
     result.results.trackmatches.track.forEach(function(element){
         tracks.push(element.name);
     });
-   return tracks;
+   return  result.results.trackmatches.track;
 }
 
 function getArtist(artist){
     var githubEndpoint = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist='+artist+'&api_key=36e606575cf4709ed9254e103e9f94c4&format=json';
+    return axios.get(githubEndpoint);
+}
+
+function getSimilarArtists(artist){
+    console.log('getSimilarArtist');
+    var githubEndpoint = 'http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist='+artist+'&api_key=36e606575cf4709ed9254e103e9f94c4&format=json';
     return axios.get(githubEndpoint);
 }
 
@@ -65,14 +90,17 @@ function searchTracks(searchText){
 }
 
 
-app.listen(app.get('port'), function(){
-    console.log('Server listening on port:', app.get('port'));
-});
-
-
 function getNames(namesList){
     var nameList = [];
     namesList.forEach(function(element, index){
 
     })
 }
+
+
+app.listen(app.get('port'), function(){
+    console.log('Server listening on port:', app.get('port'));
+});
+
+
+
